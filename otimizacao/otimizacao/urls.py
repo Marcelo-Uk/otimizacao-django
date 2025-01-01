3d160d0d5b577b808a02dc0c8917b5e79361d13d
Marcelo-Uk <mribeirocorp@gmail.com>
2024-12-31 16:47:30 -0800

    --sistema full final --closed --311224 --1647h

--- otimizacao/otimizacao/urls.py
+++ otimizacao/otimizacao/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+def redirect_to_solver(request):
+    return redirect('/solver/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('solver/', include('solver.urls')),
+    path('', redirect_to_solver),
 ]
